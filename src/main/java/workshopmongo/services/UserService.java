8398769791d78f0f8ea7371c0b90dbe2d7825c94
Romulo Wagnes <miojoblack5@gmail.comgit config --listmiojoblack5@gmail.com>
2025-08-14 15:27:45 -0400

    Obtendo um usuario por id

--- src/main/java/workshopmongo/services/UserService.java
+++ src/main/java/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import workshopmongo.domain.User;
 import workshopmongo.repository.UserRepository;
+import workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> readAll(){
 		return repo.findAll();
+	}
+	
+	public User read(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
